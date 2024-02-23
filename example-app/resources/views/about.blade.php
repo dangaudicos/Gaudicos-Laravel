42fbe13fabe2fbb6ad031a37d567937611412c80
Dannny Boy Gaudicos <beetlesprite04@gmail.com>
2024-02-24 00:54:56 +0800

    update

---
+++ example-app/resources/views/about.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=<, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <title>Home</title>
+</head>
+<body>
+    <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+        <div class="container-fluid text-center">
+            <div class="row">
+                <a class="navbar-brand mb-0 h1 col" style="color: #FFF8DC;">Second Activity Laravel</a>
+                <a class="navbar-brand mb-0 h1 col" href="/home">Home</a>
+                <a class="navbar-brand mb-0 h1 col" href="/about">About</a>
+                <a class="navbar-brand mb-0 h1 col" href="/contact">Contact Us</a>
+                <a class="navbar-brand mb-0 h1 col" href="/services">Services</a>
+            </div>
+            <button type="button" class="btn btn-outline-danger">Log out</button> 
+        </div>       
+    </nav>
+
+    <div class="text-center mt-5">
+        <h3>This is About Page</h3>
+        <a href="/home"><button type="button" class="btn btn-info">Back</button></a>
+    </div>
+
+    
+</body>
+</html>
